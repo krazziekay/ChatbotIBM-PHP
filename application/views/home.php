ceae58754c16b42f73dd8bff85ca99e59115d8ba
kAy <kay.krazzie@gmail.com>
2018-06-20 18:32:46 +1000

    Chat interface added

--- application/views/home.php
+++ application/views/home.php
@@ -1,8 +1,41 @@
 <!DOCTYPE html>
 <html xmlns="http://www.w3.org/1999/xhtml" xmlns:og="http://ogp.me/ns#" xmlns:fb="http://www.facebook.com/2008/fbml"
       lang="en" xml:lang="en">
-<head></head>
+<head><title>Chatbot</title></head>
+<link rel="stylesheet" href="<?php echo base_url('assets/chatbot-system/bootstrap/css/chat-interface.css')?>">
 <body>
-    chat bot interface here
+    <div class="wrapper">
+        <div class="container">
+
+            <div class="right">
+                <div class="top"><span><strong>Chatbot FAQ</strong></div>
+                <div class="chat" >
+                    <div class="bubble you">
+                        Hello, how are you?
+                    </div>
+                    <div class="bubble me">
+                        Hi I need some help.
+                    </div>
+                    <div class="bubble you">
+                        How may I help you?
+                    </div>
+                    <div class="bubble me">
+                        I need to know when my exam starts?
+                    </div>
+                    <div class="bubble you">
+                        The exam starts from the next week. Better start studying...
+                    </div>
+                </div>
+
+                <div class="write">
+                    <a href="javascript:;" class="write-link attach"></a>
+                    <input type="text" class="chat-text" />
+                    <button class="chat-send">Send</button>
+                    <a href="javascript:;" class="write-link smiley"></a>
+                    <a href="javascript:;" class="write-link send"></a>
+                </div>
+            </div>
+        </div>
+    </div>
 </body>
 </html>
