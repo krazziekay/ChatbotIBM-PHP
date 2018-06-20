95542f652707fdbf5d527f7d261c1caace3c3ce8
kAy <kay.krazzie@gmail.com>
2018-06-20 21:16:19 +1000

    Chat interface working

--- application/controllers/home.php
+++ application/controllers/home.php
@@ -6,6 +6,12 @@ class Home extends CI_Controller
     public function __construct()
     {
         parent::__construct();
+        $this->username = 'a01fca35-a3c5-4e9d-b9f2-18b88a06e6f9';
+        $this->password = '8jv5cqMfBA5C';
+        $this->workspace = '0b468330-0ba9-4f9d-a279-9ec6f98ddd86';
+
+        $this->messageURL='https://gateway.watsonplatform.net/assistant/api/v1/workspaces/'.$this->workspace.'/message?version=2018-02-16';
+
     }
 
     public function index()
@@ -13,6 +19,108 @@ class Home extends CI_Controller
        
         $this->load->view('home');
     }
-   
+
+
+    /**
+     * An action defined to control the click event of button with id Publish
+     * This function makes an ajax call to send the data to the IBM Watson service
+     */
+//    public function addingIntents() {
+//        $username = 'a01fca35-a3c5-4e9d-b9f2-18b88a06e6f9';
+//        $password = '8jv5cqMfBA5C';
+//        $workspace = '0b468330-0ba9-4f9d-a279-9ec6f98ddd86';
+//
+//
+//        $intent = '';
+//        $example = '';
+//        $dialog_node = $intent;
+//        $conditions = '#'.$intent;
+//
+//        $output = (object)array('text' => '');//this is the answer
+//
+//
+//        $intentResult = '';
+//
+//        if( strpos($referalLink, 'edit')) {//Edit post
+//            //First adding the intent, which is the question
+//            $dialogURL='https://gateway.watsonplatform.net/assistant/api/v1/workspaces/'.$workspace.'/dialog_nodes/'.$dialog_node.'?version=2018-02-16';
+//
+//            // After adding the intent, we now add the dialog, which is the answer
+//            $params2 = json_encode( array(
+//                'dialog_node' => $dialog_node,
+//                'conditions' => $conditions,
+//                'output' => $output,
+//                'title' => $dialog_node
+//            ) );
+//            $ch2 = curl_init();
+//            curl_setopt($ch2, CURLOPT_URL, $dialogURL);
+//            curl_setopt($ch2, CURLOPT_RETURNTRANSFER, true);
+//            curl_setopt($ch2, CURLOPT_POST, true);
+//            curl_setopt($ch2, CURLOPT_HTTPHEADER, array('Content-type: application/json'));
+//            curl_setopt($ch2, CURLOPT_POSTFIELDS, $params2);
+//            curl_setopt($ch2, CURLOPT_USERPWD, "$username:$password");
+//
+//            $dialogNodeResult=curl_exec ($ch2);
+//            curl_close ($ch2);
+//        }
+//        else {//Add post
+//            //First adding the intent, which is the question
+//            $intentURL='https://gateway.watsonplatform.net/assistant/api/v1/workspaces/'.$workspace.'/intents?version=2018-02-16';
+//            $dialogURL='https://gateway.watsonplatform.net/assistant/api/v1/workspaces/'.$workspace.'/dialog_nodes?version=2018-02-16';
+//
+//            $params1 = json_encode( array(
+//                "intent" => $intent,
+//                "examples" => array((object)["text" => $example])
+//            ) );
+//            $ch1 = curl_init();
+//            curl_setopt($ch1, CURLOPT_URL, $intentURL);
+//            curl_setopt($ch1, CURLOPT_HTTPHEADER, array('Content-type: application/json'));
+//            curl_setopt($ch1, CURLOPT_RETURNTRANSFER, true);
+//            curl_setopt($ch1, CURLOPT_POST, true);
+//            curl_setopt($ch1, CURLOPT_POSTFIELDS, $params1);
+//            curl_setopt($ch1, CURLOPT_USERPWD, "$username:$password");
+//            $intentResult=curl_exec ($ch1);
+//            curl_close ($ch1);
+//
+//
+//            // After adding the intent, we now add the dialog, which is the answer
+//            $params2 = json_encode( array(
+//                'dialog_node' => $dialog_node,
+//                'conditions' => $conditions,
+//                'output' => $output,
+//                'title' => $dialog_node
+//            ) );
+//            $ch2 = curl_init();
+//            curl_setopt($ch2, CURLOPT_URL, $dialogURL);
+//            curl_setopt($ch2, CURLOPT_RETURNTRANSFER, true);
+//            curl_setopt($ch2, CURLOPT_POST, true);
+//            curl_setopt($ch2, CURLOPT_HTTPHEADER, array('Content-type: application/json'));
+//            curl_setopt($ch2, CURLOPT_POSTFIELDS, $params2);
+//            curl_setopt($ch2, CURLOPT_USERPWD, "$username:$password");
+//
+//            $dialogNodeResult=curl_exec ($ch2);
+//            curl_close ($ch2);
+//
+//        }
+//
+//    }
+
+
+
+    public function getAnswer()
+    {
+        $params1 = json_encode($this->input->post());
+        $ch1 = curl_init();
+        curl_setopt($ch1, CURLOPT_URL, $this->messageURL);
+        curl_setopt($ch1, CURLOPT_HTTPHEADER, array('Content-type: application/json'));
+        curl_setopt($ch1, CURLOPT_RETURNTRANSFER, true);
+        curl_setopt($ch1, CURLOPT_POST, true);
+        curl_setopt($ch1, CURLOPT_POSTFIELDS, $params1);
+        curl_setopt($ch1, CURLOPT_USERPWD, "$this->username:$this->password");
+        $result = curl_exec($ch1);
+        curl_close($ch1);
+
+        echo json_encode($result);
+    }
 
 }
--- application/views/home.php
+++ application/views/home.php
@@ -2,35 +2,21 @@
 <html xmlns="http://www.w3.org/1999/xhtml" xmlns:og="http://ogp.me/ns#" xmlns:fb="http://www.facebook.com/2008/fbml"
       lang="en" xml:lang="en">
 <head><title>Chatbot</title></head>
-<link rel="stylesheet" href="<?php echo base_url('assets/chatbot-system/bootstrap/css/chat-interface.css')?>">
+<link rel="stylesheet" href="<?php echo base_url('assets/chatbot-system/bootstrap/css/chat-interface.css')?>" />
 <body>
     <div class="wrapper">
         <div class="container">
 
             <div class="right">
                 <div class="top"><span><strong>Chatbot FAQ</strong></div>
-                <div class="chat" >
-                    <div class="bubble you">
-                        Hello, how are you?
-                    </div>
-                    <div class="bubble me">
-                        Hi I need some help.
-                    </div>
-                    <div class="bubble you">
-                        How may I help you?
-                    </div>
-                    <div class="bubble me">
-                        I need to know when my exam starts?
-                    </div>
-                    <div class="bubble you">
-                        The exam starts from the next week. Better start studying...
-                    </div>
+                <div class="chat" id="chat-box">
+
                 </div>
 
                 <div class="write">
                     <a href="javascript:;" class="write-link attach"></a>
-                    <input type="text" class="chat-text" />
-                    <button class="chat-send">Send</button>
+                    <input type="text" class="chat-text" id="question" />
+                    <button class="chat-send" id="send">Send</button>
                     <a href="javascript:;" class="write-link smiley"></a>
                     <a href="javascript:;" class="write-link send"></a>
                 </div>
@@ -38,4 +24,53 @@
         </div>
     </div>
 </body>
+<script src="<?php echo base_url('assets/js/jquery-2.1.3.min.js');?>"></script>
+<script>
+    ( function(){
+
+        let calculatedHeight = $(window).height() - $('#chat-box').position().top - $('.write').height() - 92;
+        $('#chat-box').height(calculatedHeight);
+        $('#chat-box').css('overflow-y', 'scroll');
+
+
+        $('#question').keypress(function(e) {
+            if (e.keyCode === 13)
+                enter(e);
+        });
+        $('#send').click(function(){
+            enter();
+        });
+
+
+        ///////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+
+        function enter(e) {
+            $('#chat-box').scrollTop($('#chat-box')[0].scrollHeight);
+            let text = $('#question').val();
+            $('#question').val('');
+            $('#chat-box').append( $('<div class="bubble me">' + text + '</div>') );
+            $('#chat-box').append( $('<div class="bubble you loading"><div class="lds-ellipsis"><div></div><div></div><div></div><div></div></div></div>') );
+            $.ajax({
+                url: '<?php echo base_url("home/getAnswer")?>',
+                dataType: 'JSON',
+                method: 'POST',
+                data: {
+                    'input': {
+                        'text': text
+                    }
+                },
+                success: function (response) {
+                    let result = JSON.parse(response);
+                    $('.loading').remove();
+                    $('#chat-box').append( $('<div class="bubble you" id="loading">' + result.output.text + '</div>') );
+                    $('#chat-box').scrollTop($('#chat-box')[0].scrollHeight);
+                },
+                error: function (error) {
+                    console.log("Error", error);
+                }
+            });
+        }
+
+    })();
+</script>
 </html>
